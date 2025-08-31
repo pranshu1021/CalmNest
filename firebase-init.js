7253a124c98ab3493322a6db9115e28722326e99
Pranshu Gupta <159059754+pranshu1021@users.noreply.github.com>
2025-08-31 07:50:55 +0000

    some changes

---
+++ firebase-init.js
@@ -0,0 +1,16 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDGBpchBzHl03HUeUrtrcHrLLzWd-QC_II",
+    authDomain: "calmnest-4b727.firebaseapp.com",
+    projectId: "calmnest-4b727",
+    storageBucket: "calmnest-4b727.appspot.com",
+    messagingSenderId: "407039391755",
+    appId: "1:407039391755:web:df9578cfd4a24ed1fc6f0e"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
+const auth = firebase.auth();
+
